079e50bb4e1165ce52504a37a3989d0f45f2e706
Charles <16qeddy@gmail.com>
2021-03-07 00:29:45 -0600

    cleaned up code. created baseline component for MVP UI. changed searchForDomain function to work with getIpData better.
    cleaned up the code to be more logical and arguably to have better readability. current state of the application is in a play testing state. refactored helper functions to use async and await instead of callbacks and changed some variable names to create better readability. constructed baseline component for the data view to render MVP UI.

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import getIpData from '../helperFunctions/getIpData.js';
+import DataView from './dataView.js'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       domainData: {},
-       query: '136.34.193.116'
+       domainData: null,
+       query: '136.34.193.116',
+       error: false
     };
-
     this.searchForDomain = this.searchForDomain.bind(this);
+  }
+  componentDidMount(){
+    this.searchForDomain();
   }
 
   //searches for domain data with query and updates state with the new data
-  searchForDomain(){
-    getIpData(
-      this.state.query, 
-      (data)=>{
+  async searchForDomain (){
+    var data = await getIpData(this.state.query);
+    if(data === null){
+      this.setState({error: true})
+    } else{
       this.setState({
-        domainData: data
-      })
-    });
+        domainData: data,
+        error: false
+      });
+    }
   }
 
   render() {
+    if(!this.state.domainData){
+      return(null);
+    }
     return (
-      <div>
-    
+      <div className="appContainer">
+        <DataView domainData={this.state.domainData}/>
       </div>
     )
   }
